1de0b8b99ed246f2d140968f4ae05c3d476fe7da
Ivan Dorofeev <axxel123@rambler.ru>
2023-03-26 13:19:45 +0300

    Initial commit

---
+++ monitoring_console.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import logging
+
+from cprint import cprint
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+class Handler(FileSystemEventHandler):
+    path = './'
+
+    def on_any_event(self, event):
+        if not event.event_type in ['opened', 'closed', 'modified']:
+            cprint.info('Изменения в директории', event.event_type, event.src_path)
+            dir_path = self.path
+            for path, dir, file in sorted(os.walk(dir_path)):
+                if path:
+                    print(path)
+                if file:
+                    for file in sorted(file):
+                        print(' -', file)
+
+
+def main():
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    path = sys.argv[1] if len(sys.argv) > 1 else './1'
+    Handler.path = path
+
+    observer = Observer()
+    observer.schedule(Handler(), path, recursive=True)
+    observer.start()
+
+    while True:
+        try:
+            pass
+        except KeyboardInterrupt:
+            observer.stop()
+
+
+if __name__ == "__main__":
+    main()
